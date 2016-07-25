773a0e2ba395aeb6fbb8ebfd29d50b0f557cd180
Mauricio Andrades <mod.and.op@gmail.com>
2016-07-25 04:48:02 -0700

    stable; better tostring implementation

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var util = require('util');
 var traverse = require('traverse');
+var requireg = require("/Users/Op/.npm-global/lib/node_modules/requireg/lib/requireg.js")
+var javascript_stringify = requireg('javascript-stringify')
+
+var javascript_string = requireg('javascript-stringify')
 module.exports = (function () {
   return {
     log: function (args) {
@@ -9,6 +13,7 @@ module.exports = (function () {
         return console.log(JSON.stringify(el));
       });
     },
+
     ulog: function (args) {
       args = [...arguments];
       console.log('----------')
@@ -64,7 +69,7 @@ module.exports = (function () {
               this.post(function (child) {
                 if (!child.isLast)
                   s += ',';
-              });
+      javascript_stringify        });
               this.after(function () {
                 s += '}'
               });
@@ -84,5 +89,67 @@ module.exports = (function () {
       })
     },
 
+    plog: function (obj) {
+      return process.nextTick(function () {
+
+        console.log('------------')
+
+        if (typeof obj === 'string') {
+          // return setImmediate(console.log(JSON.stringify(obj)));
+          console.log(JSON.stringify(obj))
+        }
+
+        function run(_obj, callback) {
+          var scrubbed = traverse(_obj).map(function (x) {
+            if (this.circular) {
+              this.remove();
+            }
+          });
+          return callback(scrubbed);
+        }
+
+        function stringify(_obj) {
+          var s = ''
+          traverse(_obj).forEach(function to_s(node) {
+            if (Array.isArray(node)) {
+              this.before(function () {
+                s += '['
+              });
+              this.post(function (child) {
+                if (!child.isLast) s += ',';
+              });
+              this.after(function () {
+                s += ']'
+              });
+            } else if (typeof node === 'object') {
+              this.before(function () {
+                s += '{'
+              });
+              this.pre(function (x, key) {
+                to_s(key);
+                s += ':';
+              });
+              this.post(function (child) {
+                if (!child.isLast) s += ',';
+              });
+              this.after(function () {
+                s += '}'
+              });
+            } else if (typeof node === 'string') {
+              s += '"' + node.toString().replace(/"/g, '\\"') + '"';
+            } else if (typeof node === 'function') {
+              // pull function def's here.
+              s += node.toString();
+            } else {
+              s+= javascript_stringify(node);
+            }
+          });
+          return (console.log(s))
+        }
+
+        run(obj, stringify);
+
+      })
+    },
   }
 }());
