81e49eeaf4414584b983ba1c1b058268b29114af
Mauricio Andrades <mod.and.op@gmail.com>
2016-08-02 13:23:37 -0700

    closure compile

--- index.js
+++ index.js
@@ -1,160 +1,160 @@
-var util = require('util')
-var traverse = require('traverse')
-var javascript_stringify = require('javascript-stringify')
-module.exports = (function () {
+var util = require('util');
+var traverse = require('traverse');
+var javascript_stringify = require('javascript-stringify');
+module.exports = (function() {
 	return {
-		log: function (args) {
-			console.log('-----')
-			args = [...arguments]
-			return args.forEach(function (el) {
-				return console.log(JSON.stringify(el))
-			})
+		log: function(args) {
+			console.log('-----');
+			args = [...arguments];
+			return args.forEach(function(el) {
+				return console.log(JSON.stringify(el));
+			});
 		},
-		ulog: function (args) {
-			args = [...arguments]
-			return args.forEach(function (el) {
+		ulog: function(args) {
+			args = [...arguments];
+			return args.forEach(function(el) {
 				return console.log(util.inspect(el, {
 					showHidden: true,
 					depth: null
-				}))
-			})
+				}));
+			});
 		},
-		tlog: function () {
+		tlog: function() {
 			var newline = (val) => {
 				return (val ? '\n' + val : '\n');
 			};
-			var args = []
-			args.push(...arguments)
+			var args = [];
+			args.push(...arguments);
 
-			args.forEach(function (el) {
-				return setImmediate(function () {
+			args.forEach(function(el) {
+				return setImmediate(function() {
 					console.log(newline());
 					if (typeof el === 'string') {
-						return console.log(newline(JSON.stringify(el)))
+						return console.log(newline(JSON.stringify(el)));
 					} else {
-						var clone = traverse(el).clone()
-						var run = function (clone, callback) {
-							var scrubbed = traverse(clone).map(function (x) {
+						var clone = traverse(el).clone();
+						var run = function(clone, callback) {
+							var scrubbed = traverse(clone).map(function(x) {
 								if (this.circular) {
-									this.remove()
-									this.update()
+									this.remove();
+									this.update();
 								}
-							})
-							return callback(scrubbed)
-						}
-						var stringify = function (_obj) {
-							var s = ''
+							});
+							return callback(scrubbed);
+						};
+						var stringify = function(_obj) {
+							var s = '';
 							traverse(_obj).forEach(function to_s(node) {
 								if (Array.isArray(node)) {
 									if (node === null) s += 'null';
-									this.before(function () {
+									this.before(function() {
 										s += '[';
-									})
-									this.post(function (child) {
+									});
+									this.post(function(child) {
 										if (!child.isLast) s += ', ';
-									})
-									this.after(function () {
+									});
+									this.after(function() {
 										s += ']';
-									})
+									});
 								} else if (typeof node === 'object') {
 									if (node === null) {
-										s += 'null'
+										s += 'null';
 									} else {
-										this.before(function () {
+										this.before(function() {
 											s += '{';
 										});
-										this.pre(function (x, key) {
-											to_s(key)
+										this.pre(function(x, key) {
+											to_s(key);
 											s += ': ';
-										})
-										this.post(function (child) {
+										});
+										this.post(function(child) {
 											if (!child.isLast) s += ',';
-										})
-										this.after(function () {
+										});
+										this.after(function() {
 											s += '}';
-										})
+										});
 									}
 								} else if (typeof node === 'string') {
-									s += '"' + node.toString().replace(/"/g, '\\"') + '"'
+									s += '"' + node.toString().replace(/"/g, '\\"') + '"';
 								} else if (typeof node === 'function') {
-									s += javascript_stringify(node)
+									s += javascript_stringify(node);
 								} else {
-									s += javascript_stringify(node)
+									s += javascript_stringify(node);
 								}
-							})
-							console.log(s)
-						}
-						return run(el, stringify)
+							});
+							console.log(s);
+						};
+						return run(el, stringify);
 					}
-				})
-			})
+				});
+			});
 		},
-		plog: function () {
-			var args = []
-			args.push(...arguments)
+		plog : function() {
+			var args = [];
+			args.push(...arguments);
 
-			args.forEach(function (el) {
-				return process.nextTick(function () {
+			args.forEach(function(el) {
+				return process.nextTick(function() {
 					if (typeof el === 'string') {
-						return console.log(JSON.stringify(el))
+						return console.log(JSON.stringify(el));
 					} else {
-						var clone = traverse(el).clone()
-						var run = function (clone, callback) {
-							var scrubbed = traverse(clone).map(function (x) {
+						var clone = traverse(el).clone();
+						var run = function(clone, callback) {
+							var scrubbed = traverse(clone).map(function(x) {
 								if (this.circular) {
-									this.remove()
+									this.remove();
 								}
-							})
-							return callback(scrubbed)
-						}
-						var stringify = function (_obj) {
-							var s = ''
+							});
+							return callback(scrubbed);
+						};
+						var stringify = function(_obj) {
+							var s = '';
 							traverse(_obj).forEach(function to_s(node) {
 								if (Array.isArray(node)) {
 									if (node === null) {
-										s += 'null'
+										s += 'null';
 									}
-									this.before(function () {
-										s += '['
-									})
-									this.post(function (child) {
-										if (!child.isLast) s += ', '
-									})
-									this.after(function () {
-										s += ']'
-									})
+									this.before(function() {
+										s += '[';
+									});
+									this.post(function(child) {
+										if (!child.isLast) s += ', ';
+									});
+									this.after(function() {
+										s += ']';
+									});
 								} else if (typeof node === 'object') {
 									if (node === null) {
-										s += 'null'
+										s += 'null';
 									} else {
-										this.before(function () {
-											s += '{'
-										})
-										this.pre(function (x, key) {
-											to_s(key)
-											s += ': '
-										})
-										this.post(function (child) {
-											if (!child.isLast) s += ', '
-										})
-										this.after(function () {
-											s += '}'
-										})
+										this.before(function() {
+											s += '{';
+										});
+										this.pre(function(x, key) {
+											to_s(key);
+											s += ': ';
+										});
+										this.post(function(child) {
+											if (!child.isLast) s += ', ';
+										});
+										this.after(function() {
+											s += '}';
+										});
 									}
 								} else if (typeof node === 'string') {
-									s += '"' + node.toString().replace(/"/g, '\\"') + '"'
+									s += '"' + node.toString().replace(/"/g, '\\"') + '"';
 								} else if (typeof node === 'function') {
-									s += javascript_stringify(node)
+									s += javascript_stringify(node);
 								} else {
-									s += javascript_stringify(node)
+									s += javascript_stringify(node);
 								}
-							})
-							console.log(s)
-						}
-						return run(el, stringify)
+							});
+							console.log(s);
+						};
+						return run(el, stringify);
 					}
-				})
-			})
+				});
+			});
 		}
 	}
 }())
